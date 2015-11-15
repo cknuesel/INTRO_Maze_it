6c4f110676f5285231b473fc69a4facf3409eecf
cknuesel <c_k_@bluewin.ch>
2015-11-15 20:06:07 +0100

    version i2c

--- PCB_robo/Sources/Platform_Local_Version.h
+++ PCB_robo/Sources/Platform_Local_Version.h
@@ -9,7 +9,7 @@
 #define SOURCES_PLATFORM_LOCAL_VERSION_H_
 
 
-/* Robo version defined */
+/* Robo version defined   */
 #define MCP4728_I2C_ADDR  	(0) /* 0 = Robo Cyrill Addr 0x60; 1 = Robo Manuel Addr 0x61 */
 
 
